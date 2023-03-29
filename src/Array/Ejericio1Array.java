43abcf2def728003f48307623f58aed7051753c0
j.romero.sanchez <juaki00@gmail.com>
2023-03-29 10:02:46 +0200

    Añado Ejercicio 1 con array mejorado

--- src/Array/Ejericio1Array.java
+++ src/Array/Ejericio1Array.java
@@ -8,7 +8,9 @@ primera o en la última posición hay un 6, y falso (false) si no.
 public class Ejericio1Array {
 
     private int[] array;
-    public Ejericio1Array() {
+
+    public Ejericio1Array(int[] array) {
+        this.array = array;
     }
 
     public boolean empiezaTerminaEn6(int[] a) {
@@ -19,8 +21,8 @@ public class Ejericio1Array {
         int[] arrayNull = null;
         int[] arrayVacio = new int[0];
         int[] array = {1, 2, 3, 4, 5, 1};
-        Ejericio1Array e1 = new Ejericio1Array();
+        Ejericio1Array e1 = new Ejericio1Array(array);
 
-        System.out.println(e1.empiezaTerminaEn6(array));
+        System.out.println(e1.empiezaTerminaEn6(e1.array));
     }
 }
